8daf734e11fda53ff35912bce2a82a252e1c739e
Sven Lüchow <sven.luechow@icloud.com>
2024-06-05 17:53:16 +0200

    update gitignore and languagecode.ts

--- src/types/LangaugeCode.ts
+++ src/types/LangaugeCode.ts
@@ -20,4 +20,23 @@ export const LanguageCodeEnumSchema = z.preprocess(
   (_) => LanguageCode.en,
   z.nativeEnum(LanguageCode).default(LanguageCode.en),
 );
-export type LangaugeCodeEnum = z.infer<typeof LanguageCodeEnumSchema>;
+export type LanguageCodeEnum = z.infer<typeof LanguageCodeEnumSchema>;
+
+
+
+export const LanguageTranslations = {
+  en: "English",
+  de: "Deutsch",
+  jp: "日本語",
+  cn: "简体中文",
+  kr: "한국어",
+  pt: "Português",
+  ru: "Pусский язык",
+  id: "Bahasa Indonesia",
+  vi: "Tiếng Việt",
+  th: "ภาษาไทย",
+  es: "Español",
+  fr: "Français"
+} as const;
+
+export const LanguageTranslationArray = Object.entries(LanguageTranslations).map(([key, value]) => ({ key, value }));
